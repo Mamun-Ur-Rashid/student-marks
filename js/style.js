c7dcd35d70746b656069f190fa10b32a90263e72
Mamun-Ur-Rashid <engmamun656@gmail.com>
2025-05-20 18:23:25 +0600

    using search and find method

--- js/style.js
+++ js/style.js
@@ -6,3 +6,22 @@ const Students =[
     { id: 4, name: "David", marks: 25},
     { id: 5, name: "Eva", marks: 90},
 ]
+
+function searchStudent(params) {
+    const nameValue = document.getElementById("searchName").value.toLowerCase();
+    const foundedData = Students.find((student) => student.name.toLocaleLowerCase() === nameValue);
+    const resultDiv = document.getElementById("searchResult");
+
+    if (foundedData) {
+        resultDiv.innerHTML = `
+        <div class="student"> Found: ${foundedData.name} (Marks: ${foundedData.marks}) </div>
+        `
+    } else {
+        resultDiv.innerHTML = `
+        <div class="student"> No student found with Name ${nameValue}</div>
+        `
+    }
+    
+
+    console.log(foundedData);
+}
