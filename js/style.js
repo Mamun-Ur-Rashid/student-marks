1fd4707491b451fdf51af3ebf6a29e9ea0e3e271
Mamun-Ur-Rashid <engmamun656@gmail.com>
2025-05-20 17:21:41 +0600

    first commit

---
+++ js/style.js
@@ -0,0 +1,8 @@
+
+const Students =[
+    { id: 1, name: "Alice", marks: 85},
+    { id: 2, name: "Bob", marks: 35},
+    { id: 3, name: "Charlie", marks: 35},
+    { id: 4, name: "David", marks: 25},
+    { id: 5, name: "Eva", marks: 90},
+]
